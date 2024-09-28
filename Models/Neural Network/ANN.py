802913b7634b22c6bf2df4ef35fd361bdcaf54e3
ArtinABS <artin5757@gmail.com>
2024-09-28 21:47:48 +0330

    Test the model with MNIST dataset

--- Models/Neural Network/ANN.py
+++
@@ -1,5 +0,0 @@
-import keras
-from keras.api.layers import Dense, Conv3D, MaxPooling3D, BatchNormalization, ReLU, Dropout
-from keras.api.models import Sequential
-import matplotlib.pyplot as plt
-import numpy as np
